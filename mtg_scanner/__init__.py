6a0168775502f525d4e35cb015d5a5754996aced
Mark Walker <markwal@hotmail.com>
2022-11-23 11:44:44 -0800

    Attempt to canonicalize using scryfall

--- mtg_scanner/__init__.py
+++ mtg_scanner/__init__.py
@@ -1,7 +1,10 @@
+import logging
+logging.basicConfig(format='%(levelname)s\t%(message)s', level=logging.WARN)
+
 import click
 import cv2
 from mtg_scanner import card
-import logging
+from mtg_scanner.scryfall import canonicalizeCard
 
 @click.command()
 
@@ -11,8 +14,10 @@ import logging
 
 def main(image, output, debug):
     logging.basicConfig(format='%(levelname)s\t%(message)s', 
-            level=logging.DEBUG if debug else logging.WARN)
+            level=logging.INFO if debug else logging.WARN, force=True)
+    logging.getLogger("root").setLevel(logging.DEBUG if debug else logging.WARN)
     for fn in image:
+        logging.info(f'reading {fn}')
         try:
             img = cv2.imread(fn)
             if img is None:
@@ -21,8 +26,16 @@ def main(image, output, debug):
             logging.warn(f'Unable to read image <{fn}>')
             continue
 
+        logging.info(f'recognizing {fn}')
         c = card.StraightCard(img, card_type=None, save_debug_images=debug)
-        print(f'1 {c.read_title(90)} ({c.read_set_code()}) {c.read_collector_number()}', file=output)
+        title = c.read_title(90)
+        set_code = c.read_set_code()
+        collector_number = c.read_collector_number()
+        logging.info(f'Recognizer returned: {title} ({set_code}) {collector_number}')
+        rval, cs = canonicalizeCard(title, set_code, collector_number)
+
+        logging.info(f'Canonicalized: {rval}\t{cs}')
+        print(f'{cs}', file=output)
 
 if __name__ == '__main__':
     main()
--- mtg_scanner/scryfall.py
+++ mtg_scanner/scryfall.py
@@ -1,21 +1,32 @@
 import requests
 import json
 from urllib.parse import quote_plus
+import logging
 
-def CanonicalizeCard(title, set_code=None, collector_number=None):
+def canonicalizeCard(title, set_code=None, collector_number=None):
+    logging.info("calling scryfall")
     if set_code and collector_number:
         # try and look up by set_code and collector_number first
-        set_code = set_code.split("/")[0]
+        set_code = set_code.casefold()
+        collector_number = collector_number.split("/")[0]
+        logging.info(f'set_code: {set_code}')
+        logging.info(f'collector_number: {collector_number}')
         if len(set_code) in range(1, 4) and len(collector_number) in range(1, 4) and \
                 set_code.isalnum() and collector_number.isdigit():
-            response = requests.get(f'https://api.scryfall.com/cards/{set_code}/{collector_number}')
+            url = f'https://api.scryfall.com/cards/{set_code}/{collector_number}'
+            logging.info(f'GET {url}')
+            response = requests.get(url)
+            logging.info(f'response.status = {response.status_code}')
             if response.status_code == 200:
                 card = response.json()
                 if "name" in card and card["name"] == title:
                     return True, f'{title} ({set_code}) {collector_number}'
 
     # didn't find an exact match via set code and collector number
-    response = requests.get(f'https://api.scryfall.com/cards/named?fuzzy={quote_plus(title)}')
+    url = f'https://api.scryfall.com/cards/named?fuzzy={quote_plus(title)}'
+    logging.info(f'fuzzy: GET {url}')
+    response = requests.get(url)
+    logging.info(f'response.status = {response.status_code}')
     if response.status_code != 200:
         return False, title
     card = response.json()
@@ -23,10 +34,12 @@ def CanonicalizeCard(title, set_code=None, collector_number=None):
         return False, title
     title = card["name"]
 
-    if not "prints_search_uri" in card:
+    if not "prints_search_uri" in card or set_code is None or collector_number is None:
         return "fuzzy-title", title
 
+    logging.info(f'prints: GET {card["prints_search_uri"]}')
     response = requests.get(card["prints_search_uri"])
+    logging.info(f'response.status = {response.status_code}')
     if response.status_code != 200:
         return "fuzzy-title", title
 
@@ -35,25 +48,14 @@ def CanonicalizeCard(title, set_code=None, collector_number=None):
 
     candidate = None
     for card in data:
-        if "set" in card and card["set"] == set_code:
+        if "set" in card and card["set"].casefold() == set_code:
             candidate = card
 
-            if 
+            if "collector_number" in card and card["collector_number"] == collector_number:
+                return True, f'{title} ({card["set"]}) {card["collector_number"]}'
 
+    if candidate:
+        return "fuzzy-title-set", f'{title} ({candidate["set"]}) {candidate["collector_number"]}'
 
+    return "fuzzy-title", title
 
-response = requests.get("https://api.scryfall.com/cards/named?fuzzy=muldrotha")
-print(response.status_code)
-card = response.json()
-if "name" in card:
-    print(card["name"])
-print(card["set"])
-print(card["collector_number"])
-
-uri = card["prints_search_uri"]
-response = requests.get(uri)
-cards = response.json()
-
-print(json.dumps(cards, indent=4))
-for obj in cards["data"]:
-    print(obj["set"], obj["collector_number"])
